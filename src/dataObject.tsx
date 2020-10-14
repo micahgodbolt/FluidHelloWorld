c9aca19365cd6caec46fcf9b9c7daa1a17d120ea
Micah Godbolt <mgodbolt@microsoft.com>
2020-10-14 09:46:00 -0700

    combined form control

--- src/dataObject.tsx
+++ src/dataObject.tsx
@@ -7,18 +7,16 @@ import { SharedMap } from "@fluidframework/map";
 import { createListItems, IExampleItem } from '@uifabric/example-data';
 
 interface IFluentList {
-    items: IExampleItem[]
-    updateItemLocation: (itemKey: string, value: string ) => void
-    updateItemHeight: (itemKey: string, value: string ) => void
+    deleteItem: (itemKey: string) => void
+    createItem: (item: IExampleItem) => void
 }
 
-export class FluentList extends DataObject implements IFluentList  {
+export class FluentList extends DataObject implements IFluentList {
     private myMap: SharedMap | undefined;
-
 
     protected async initializingFirstTime() {
         const items = SharedMap.create(this.runtime);
-        createListItems(10).forEach((v:IExampleItem) => items.set(v.key, v));
+        createListItems(10).forEach((v: IExampleItem) => items.set(v.key, v));
         this.root.set("fluentlist", items.handle);
     }
 
@@ -34,19 +32,42 @@ export class FluentList extends DataObject implements IFluentList  {
         return Array.from(this.myMap.values());
     }
 
-    public readonly updateItemLocation = (itemKey: string, value: string) => {
+
+    public get itemKeys() {
+        if (!this.myMap) {
+            throw new Error("Map not initialized");
+        }
+        return Array.from(this.myMap.keys());
+    }
+
+
+    private updateItemLocation = (itemKey: string, value: string) => {
         if (this.myMap) {
-            const newItem = {...this.myMap.get(itemKey), location: value}
+            const newItem = { ...this.myMap.get(itemKey), location: value }
             this.myMap?.set(itemKey, newItem)
         }
     };
 
-    public readonly updateItemHeight = (itemKey: string, value: string) => {
+    private updateItemHeight = (itemKey: string, value: string) => {
         if (this.myMap) {
-            const newItem = {...this.myMap.get(itemKey), height: value}
+            const newItem = { ...this.myMap.get(itemKey), height: value }
             this.myMap?.set(itemKey, newItem)
         }
     };
+
+    public location = (itemKey: string) => {
+        return {
+            get: () => this.myMap?.get(itemKey).location,
+            set: (value: string) => this.updateItemLocation(itemKey, value)
+        }
+    }
+
+    public height = (itemKey: string) => {
+        return {
+            get: () => this.myMap?.get(itemKey).height,
+            set: (value: string) => this.updateItemHeight(itemKey, value)
+        }
+    }
 
     public readonly deleteItem = (itemKey: string) => {
         if (this.myMap && this.myMap.get(itemKey)) {
--- src/view.tsx
+++ src/view.tsx
@@ -4,61 +4,66 @@ import { IExampleItem, createListItems } from '@uifabric/example-data';
 
 initializeIcons();
 
-export const FluentListView = ({model} : {model: any}) => {
-  const { items, updateItemLocation, updateItemHeight, deleteItem, createItem, myMap } = model;
-  const [localItems, setItems] = React.useState(items);
+export const FluentListView = ({ model }: { model: any }) => {
+  const { itemKeys, location, height, deleteItem, createItem, myMap } = model;
+  const [localItems, setItems] = React.useState(itemKeys);
 
   React.useEffect(() => {
     myMap.on("valueChanged", () => {
-      setItems(model.items)
+      console.log('change')
+      setItems(model.itemKeys)
     });
-}, [myMap]);
+  }, [myMap]);
 
 
 
 
   const onRenderCell = (item?: IExampleItem) => {
+    const itemHeight = height(item);
+    const itemLocation = location(item);
+
     const dropdownProps: IDropdownProps = {
-      styles:{root:{display: 'inline-block', minWidth: 300}},
-      options:[
-        {key: 'Los Angeles', text: 'Los Angeles'},
-        {key: 'New York', text: 'New York'},
-        {key: 'Portland', text: 'Portland'},
-        {key: 'Seattle', text: 'Seattle'},
-        {key: 'Chicago', text: 'Chicago'}
+      styles: { root: { display: 'inline-block', minWidth: 300 } },
+      options: [
+        { key: 'Los Angeles', text: 'Los Angeles' },
+        { key: 'New York', text: 'New York' },
+        { key: 'Portland', text: 'Portland' },
+        { key: 'Seattle', text: 'Seattle' },
+        { key: 'Chicago', text: 'Chicago' }
       ],
-      selectedKey: item?.location, 
+      selectedKey: itemLocation.get(),
       onChange: (event: React.FormEvent<HTMLDivElement>, option: IDropdownOption | undefined): void => {
-        updateItemLocation(item?.key, option?.key)
+        itemLocation.set(option?.key)
       }
     }
-  
+
     if (!item) {
       return null;
     }
-    return(
-    <div style={{display: 'flex'}}>
-      <Dropdown  {...dropdownProps} /> 
-      <Slider 
-        styles={{root: {flexGrow: 1}}} 
-        value={item.height} 
-        min={100} 
-        max={300} 
-        onChanged={(e, value) => updateItemHeight(item?.key, value)}  
-      />
-      <IconButton onClick={() => deleteItem(item.key) } iconProps={{iconName: 'Delete'}} />   
+    return (
+      <div style={{ display: 'flex' }}>
+        <Dropdown  {...dropdownProps} />
+        <Slider
+          styles={{ root: { flexGrow: 1 } }}
+          value={itemHeight.get()}
+          min={100}
+          max={300}
+          onChanged={(e, value) => itemHeight.set(value)}
+
+        />
+        <IconButton onClick={() => deleteItem(item)} iconProps={{ iconName: 'Delete' }} />
       </div>
     )
   }
 
 
   return (
-  <div>
-      {localItems.length > 0 ? <List items={localItems} onRenderCell={onRenderCell} /> : <></>}<br/><br/>
-      <IconButton onClick={() =>  createItem(createListItems(1)[0])} iconProps={{iconName:"Add"}} />
-  </div>
+    <div>
+      {localItems.length > 0 ? <List items={localItems} onRenderCell={onRenderCell} /> : <></>}<br /><br />
+      <IconButton onClick={() => createItem(createListItems(1)[0])} iconProps={{ iconName: "Add" }} />
+    </div>
   )
-  
+
 }
 
 
